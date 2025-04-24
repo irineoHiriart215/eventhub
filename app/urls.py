9ce819044027374abd355a2cd2c91358da5a0b71
jjmvicente <juamar1306@gmail.com>
2025-04-24 20:53:03 -0300

    [Feature] Agregamos rutas para gestionar comentarios

--- app/urls.py
+++ app/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
     path("events/<int:id>/edit/", views.event_form, name="event_edit"),
     path("events/<int:id>/", views.event_detail, name="event_detail"),
     path("events/<int:id>/delete/", views.event_delete, name="event_delete"),
+    #rutas para los comentarios
+    path("events/<int:event_id>/comments/add/", views.add_comment, name="add_comment"),
+    path("comments/<int:comment_id>/edit/", views.edit_comment, name="edit_comment"),
+    path("comments/<int:comment_id>/delete/", views.delete_comment, name="delete_comment"),
+
 ]
