69e1d08735167a0b09aad615d65bce2b3e0b9724
Tomas Battistella <tomas.battistella0@gmail.com>
2025-04-30 20:33:59 -0300

    Se agrego el modelo Ticket en models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -87,3 +88,41 @@ class Comment(models.Model):
     def can_user_delete(self, user):
         return self.user == user or self.event.organizer == user
 
+class Ticket(models.Model):
+# Definimos un choice field para los tipos de tickets
+    TICKET_TYPES = [ 
+        ("GENERAL", "General"),
+        ("VIP", "VIP"),
+    ]
+# Atributos
+    buy_date = models.DateTimeField(auto_now_add=True)
+    ticket_code = models.CharField(max_length="12", unique=True, editable=False)
+    quantity = models.IntegerField()
+    type = models.CharField(max_length="10", choices=TICKET_TYPES)
+
+# Relaciones muchos a uno
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tickets")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="tickets")
+
+# Metodos
+    def save(self, *args,**kwargs):
+        """Se asegura de que se haya generado el code antes de guardar"""
+        if not self.ticket_code:
+            self.ticket_code = self.generate_ticket_code()
+        super().save(*args,**kwargs)
+
+    def generate_ticket_code(self):
+        return uuid.uuid4().hex[:12].upper()
+    
+    def can_be_modified_by_user(self, user):
+        """Permite editar o eliminar si es el dueño del ticket"""
+        return self.user == user
+    
+    def can_be_deleted_by_organizer(self, user):
+        """Permite eliminar si es el organizador del evento"""
+        return self.event.organizer == user
+    
+    def __str__(self):
+        """Cuando se imprima un objeto en especifico se vera de la siguiente forma: VIP x2 - juanito - A1B2C3D4E5F6"""
+        return f"{self.type} x{self.quantity} - {self.user.username} - {self.ticket_code}"
+    
